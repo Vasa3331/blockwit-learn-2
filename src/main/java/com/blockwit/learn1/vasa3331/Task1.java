72652d68417d304722b118f4916c624370418fb4
vasa3331 <vasiliymartinov@gmail.com>
2022-01-27 00:39:06 +0300

    Task1 finished

--- src/main/java/com/blockwit/learn1/vasa3331/Task1.java
+++ src/main/java/com/blockwit/learn1/vasa3331/Task1.java
@@ -1,7 +1,16 @@
 package com.blockwit.learn1.vasa3331;
 
-public class Task1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+import java.util.Scanner;
+
+    public class Task1 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите Ваш год рождения?");
+            int year = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Введите Ваше Имя?");
+            String name = sc.nextLine();
+            System.out.println(name + " ,вам: " + (2022 - year) + " лет");
+        }
     }
-}
--- src/main/java/com/blockwit/learn1/vasa3331/Task2.java
+++ src/main/java/com/blockwit/learn1/vasa3331/Task2.java
@@ -1,7 +1,13 @@
 package com.blockwit.learn1.vasa3331;
-
+import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите Ваш год рождения");
+        int year = sc.nextInt();
+        System.out.print("Введите Ваше Имя");
+        String name = sc.nextLine();
+        sc.close();
+        System.out.print(name + " ,вам: " + (2022 - year) + " лет");
     }
 }
