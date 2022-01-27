63e8058637dd19420e9bbafca719934c35c990d5
vasa3331 <vasiliymartinov@gmail.com>
2022-01-27 23:05:01 +0300

    Task5 finished

---
+++ src/main/java/com/blockwit/learn2/vasa3331/Task5.java
@@ -0,0 +1,18 @@
+package com.blockwit.learn2.vasa3331;
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter 4 numbers");
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String c = sc.nextLine();
+        String d = sc.nextLine();
+        StringBuilder sb = new StringBuilder(d + b + c + a);
+        System.out.println(sb);
+
+    }
+}
+
+
