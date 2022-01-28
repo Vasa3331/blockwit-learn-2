0acc82550adcc0cc2b2e1d8f6ad44e4bb9d9a9b0
vasa3331 <vasiliymartinov@gmail.com>
2022-01-28 22:03:13 +0300

    tu1 with Helper finished

---
+++ src/main/java/com/blockwit/learn2/vasa3331/Helper.java
@@ -0,0 +1,15 @@
+package com.blockwit.learn2.vasa3331;
+
+public class Helper {
+    int[] a = {12, 7, 89, -4, 8, 9};
+
+    public void printIntArray() {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Helper mass = new Helper();
+        mass.printIntArray();
+    }
+}
