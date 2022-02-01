b854adf385fb618cb17b8514bb7221157a0e6108
vasa3331 <vasiliymartinov@gmail.com>
2022-02-01 10:18:49 +0300

    Helper and tu0 changed

--- src/main/java/com/blockwit/learn2/vasa3331/Helper.java
+++ src/main/java/com/blockwit/learn2/vasa3331/Helper.java
@@ -1,15 +1,10 @@
 package com.blockwit.learn2.vasa3331;
 
 public class Helper {
-    int[] a = {12, 7, 89, -4, 8, 9};
 
-    public void printIntArray() {
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
+    public void printIntArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
         }
-    }
-    public static void main(String[] args) {
-        Helper mass = new Helper();
-        mass.printIntArray();
     }
 }
--- src/main/java/com/blockwit/learn2/vasa3331/tu0.java
+++ src/main/java/com/blockwit/learn2/vasa3331/tu0.java
@@ -1,16 +1,23 @@
 package com.blockwit.learn2.vasa3331;
-import java.util.*;
 
 public class tu0 {
     public static void main(String[] args) {
-        int[] a = {12, 7, 89, -4, 8, 9};
+        int[] array = {12, 7, 89, -4, 8, 9};
         for (int i = 0; i < 6; i++) {
-            System.out.print(a[i] + " ");
+            System.out.print(array[i] + " ");
         }
-        Arrays.sort(a);
-        System.out.println();
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while (j >= 0 && current < array[j]) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+            }
+        System.out.println("");
         for (int i = 0; i < 6; i++) {
-            System.out.print(a[i] + " ");
+            System.out.print(array[i] + " ");
         }
     }
 }
