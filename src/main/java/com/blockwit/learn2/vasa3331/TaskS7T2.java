bd8a63ca8bd19115caa86ac0aef6dc4c7b6c3a2b
vasa3331 <vasiliymartinov@gmail.com>
2022-02-01 12:27:03 +0300

    TaskS7T2 changed

--- src/main/java/com/blockwit/learn2/vasa3331/TaskS7T2.java
+++ src/main/java/com/blockwit/learn2/vasa3331/TaskS7T2.java
@@ -1,25 +1,10 @@
 package com.blockwit.learn2.vasa3331;
 
 public class TaskS7T2 {
-    public static int [] a = {6, 3, 8, 9};
-    public static int [] b = {0, 3, 1, 5};
-    public static int [] res;
-
-    public void printIntArray() {
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-            }
-        System.out.println(" - Массив a");
-        for (int i = 0; i < b.length; i++) {
-            System.out.print(b[i] + " ");
-        }
-        System.out.println(" - Массив b");
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i] + " ");
-        }
-        System.out.println(" - Массив res");
-    }
     public static void main(String[] args) {
+        int [] a = {6, 3, 8, 9};
+        int [] b = {0, 3, 1, 5};
+        int [] res;
         res = new int[a.length + b.length];
         for (int i = 0; i < res.length; i = i + 2) {
             res[i] = a[i/2];
@@ -27,7 +12,14 @@ public class TaskS7T2 {
         for (int j = 0; j < b.length; j++) {
             res[j * 2 + 1] = b[j];
         }
-        TaskS7T2 mass = new TaskS7T2();
-        mass.printIntArray();
+        Helper mass1 = new Helper();
+        mass1.printIntArray(a);
+        System.out.println(" - Массив a");
+        Helper mass2 = new Helper();
+        mass2.printIntArray(b);
+        System.out.println(" - Массив b");
+        Helper mass3 = new Helper();
+        mass3.printIntArray(res);
+        System.out.println(" - Массив res");
     }
 }
