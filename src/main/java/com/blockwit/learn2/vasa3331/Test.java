30dc50eb14088f64d39847145d6af5e978c92158
vasa3331 <vasiliymartinov@gmail.com>
2022-02-08 10:28:39 +0300

    TaskS8T3 + Test finished

--- src/main/java/com/blockwit/learn2/vasa3331/Test.java
+++ src/main/java/com/blockwit/learn2/vasa3331/Test.java
@@ -1,10 +1,17 @@
 package com.blockwit.learn2.vasa3331;
 
+import java.util.Random;
+
 public class Test {
     public static void main(String[] args) {
         MyIntArrayList list = new MyIntArrayList();
-        for (int i = 0; i < 999; i++) {
+        for (int i = 0; i < 11; i++) {
             list.add(i);
+        }
+        Random r = new Random();
+        for (int i = 0; i < 3; i++) {
+            int x = r.nextInt();
+            list.add(x);
         }
         System.out.println(list.size());
         Helper helper = new Helper();
